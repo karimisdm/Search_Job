6e2ea3602319e358211f2e5989103183384a470a
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-06-06 23:37:19 +0200

    updated

--- search-job/src/components/MainNav.vue
+++ search-job/src/components/MainNav.vue
@@ -1,4 +1,5 @@
 <script setup>
+const companyName = "MyCompany Careers";
 </script>
 
 <template>
@@ -6,7 +7,7 @@
         <div class="fixed top-0 left-0 h-16 w-full bg-white">
             <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
                 <a href="#" class="flex h-full items-center align-middle text-xl">
-                    SMNet Careers
+                    {{companyName}}
                 </a>
 
             </div>
