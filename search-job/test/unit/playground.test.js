698c3cd4b5a4a38172db332c9c22121c07dd33c0
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-06-23 23:55:48 +0200

    updated

--- search-job/test/unit/playground.test.js
+++ search-job/test/unit/playground.test.js
@@ -1,7 +1,14 @@
 import {describe, it, expect} from 'vitest';
+import {evenOrOdd} from '@/playground.js';
 
 describe("basic math", ()=>{
     it("add two numbers", ()=>{
         expect(1+1).toBe(2);
+    })
+});
+
+describe("evenOrOdd", ()=>{
+    it("indicates the number is even or odd", ()=>{
+        expect(evenOrOdd(3)).toBe("Odd");
     })
 })
