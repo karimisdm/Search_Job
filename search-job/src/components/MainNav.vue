42c8fced59bed6b16350562501e7741d54b5c3b3
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-06-25 23:52:46 +0200

    updated

--- search-job/src/components/MainNav.vue
+++ search-job/src/components/MainNav.vue
@@ -1,30 +1,35 @@
 <script setup>
-const companyName = "MyJobMarket";
-const url = "http://careers.google.com";
-const menuItems = ["Teams", "Jobs", "Locations", "Life at MyJobMarket", "How we hire", "Students"];
+const companyName = "MyJobMarket"
+const url = "http://careers.google.com"
+const menuItems = [
+  "Teams",
+  "Jobs",
+  "Locations",
+  "Life at MyJobMarket",
+  "How we hire",
+  "Students"
+]
 </script>
 
 <template>
-    <header class="w-full text-sm">
-        <div class="fixed top-0 left-0 h-16 w-full bg-white">
-            <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-                <a :href= 'url' class="flex h-full items-center align-middle text-xl">
-                    {{companyName}}
-                </a>
-                <nav class="ml-12 h-full">
-                    <ul class="flex h-full list-none">
-                        <li v-for="item in menuItems" :key="item.id"  class="ml-9 h-full" >
-                            <a href="" class="flex h-full items-center py-2.5 ">
-                                {{ item }}
-                            </a>
-                        </li>
-                  
-                 
-                    </ul>
-                </nav>
-
-            </div>
-        </div>
-    </header>
-
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+      <div class="mx-auto flex items-center h-16 border-b border-solid border-gray-300 px-8">
+        <a :href="url" class="flex items-center text-xl">
+          {{ companyName }}
+        </a>
+        <nav class="ml-12">
+          <ul class="flex list-none">
+            <li
+              v-for="item in menuItems"
+              :key="item"
+              class="ml-9 flex items-center first:ml-0"
+            >
+              <a href="" class="flex items-center py-2.5">{{ item }}</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 </template>
