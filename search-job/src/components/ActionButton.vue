c0dd9319bbcc07efb86b2d45b47218dba62dae78
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-07-01 20:20:23 +0200

    Using Props for sign-in button

--- search-job/src/components/ActionButton.vue
+++ search-job/src/components/ActionButton.vue
@@ -1,9 +1,10 @@
 <script setup>
 const handleClick = ()=>{
     console.log("Button Clicked!");
-}
+};
+defineProps(["text"]);
 </script>
 
 <template>
-  <button class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue">Sign In</button>
+  <button class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue">{{ text }}</button>
 </template>
