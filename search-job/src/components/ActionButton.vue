39b99593a4f6a95c68d73b10af4a3185ac418210
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-07-01 21:20:12 +0200

    add some custom classes in Tailwind

--- search-job/src/components/ActionButton.vue
+++ search-job/src/components/ActionButton.vue
@@ -6,5 +6,20 @@ defineProps(["text"]);
 </script>
 
 <template>
-  <button class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue">{{ text }}</button>
+  <button class="rounded border-0 bg-brand-blue-1 text-white  px-5 py-3 font-medium hover:shadow-blue ">{{ text }}</button>
 </template>
+
+<style scoped>
+button {
+  @apply rounded px-5 py-3 font-medium ;
+}
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+}
+.secondary {
+  @apply 
+
+}
+
+
+</style>
