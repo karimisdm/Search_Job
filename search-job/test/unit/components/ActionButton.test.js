04c5e7b35ed5e1fed608eedfda83cbcca4f3cde3
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-07-02 23:59:39 +0200

    added test for ActionButton

---
+++ search-job/test/unit/components/ActionButton.test.js
@@ -0,0 +1,22 @@
+import { describe, it , expect } from "vitest"
+import {render, screen} from '@testing-library/vue'
+import '@testing-library/jest-dom'
+import ActionButton from '@/components/ActionButton.vue'
+
+
+describe('ActionButton',()=>{
+    it('render text',()=>{
+        render(ActionButton, {
+            props:{
+                text: 'Sign in',
+                type:'primary'
+            }
+        })
+        const button = screen.getByRole('button', {
+            name:/Sign in/i
+        });
+        expect(button).toBeInTheDocument();
+        
+    });
+
+})
