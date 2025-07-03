6d4d6506e22d47b5af9c39715c7d9e8656fd6ed2
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-07-03 19:12:25 +0200

    added search icon

--- search-job/src/components/TheSubNav.vue
+++ search-job/src/components/TheSubNav.vue
@@ -3,7 +3,11 @@
 <template>
     <div class="w-full h-16 bg-white border-b border-solid border-brand-gray-1">
         <div class="flex h-full items-center px-8">
-              This is The sun nav!
+              <div>
+                <font-awesome-icon :icon="['fas','search']" class="mr-3"/>
+                <span><span class="text-brand-green-1">1359</span> jobs matched</span>
+
+              </div>
         </div>
         
     </div>
--- search-job/src/main.ts
+++ search-job/src/main.ts
@@ -2,14 +2,20 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 import '@/assets/main.css'
 import '@/index.css'
 import '@/output.css'
 import App from '@/App.vue'
 import router from '@/router'
 
+library.add(faSearch);
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
