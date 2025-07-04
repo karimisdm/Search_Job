67907fe8a88a6b0671dbb1f0cb26dd70c5d4621a
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-07-04 22:48:39 +0200

    added css grid to TheHero

--- search-job/src/components/TheHero.vue
+++ search-job/src/components/TheHero.vue
@@ -2,8 +2,23 @@
 </script>
 <template>
     <main>
-        <section>
-            I can be hero!
+        <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">
+            <div class="grid grid-cols-12">
+                <div class="col-span-1 col-start-1 border border-blue-400">
+                  col 1
+                </div>
+                <div class="col-span-5 col-start-2 border border-blue-400">
+                    col 5
+
+                </div>
+                <div class="col-span-5 col-start-7 border border-blue-400">
+                    col 5
+
+                </div>
+                <div class="col-span-1 col-start-12 border border-blue-400">
+                    col 1
+                </div>
+            </div>
         </section>
     </main>
 </template>
