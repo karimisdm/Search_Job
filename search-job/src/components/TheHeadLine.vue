ea468b36400ae04777ff019fa569d584c23fbd39
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-07-04 23:59:49 +0200

    added Headline component

---
+++ search-job/src/components/TheHeadLine.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {onMounted} from 'vue'
+
+onMounted(()=>{
+    console.log('TheHeadLine component mounted')
+})
+
+
+</script>
+<template>
+    <section>
+        <h1>
+            Build for everyone
+        </h1>
+        <h2>
+             Find your next job at MyCompany
+        </h2>
+    </section>
+</template>
