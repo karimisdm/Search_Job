264e87d83cdf95625fcd69308fc949fb9d93a96d
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-07-08 23:02:06 +0200

    added beforeEach , afterEach to test

--- search-job/test/unit/components/TheHeadLine.test.js
+++ search-job/test/unit/components/TheHeadLine.test.js
@@ -3,27 +3,32 @@ import { render, screen } from "@testing-library/vue";
 import TheHeadLine from "@/components/TheHeadLine.vue";
 import { nextTick } from "vue";
 import '@testing-library/jest-dom';
+import { afterEach, beforeEach } from "node:test";
 
 describe('TheHeadLine', ()=>{
-   it('display introductory action verb', ()=>{
+   beforeEach(()=>{
     vi.useFakeTimers();
+   });
+   afterEach(()=>{
+    vi.useRealTimers();
+   })
+
+
+
+   it('display introductory action verb', ()=>{
     render(TheHeadLine);
 
     const actionPhrase = screen.getByRole('heading', {
         name: /build for everyone/i
     });
     expect(actionPhrase).toBeInTheDocument();
-    vi.useRealTimers();
    });
 
    it('changes action verb at a interval', ()=>{
-
-    vi.useFakeTimers();
     const mock = vi.fn();
     vi.stubGlobal('setInterval', mock);
     render(TheHeadLine);
     expect(mock).toHaveBeenCalledTimes(1);
-    vi.useRealTimers();
    });
 
    it('swap action verb after interval', async()=>{
@@ -37,17 +42,18 @@ describe('TheHeadLine', ()=>{
     });
     expect(actionPhrase).toBeInTheDocument();
     vi.useRealTimers();
-   });
+   
+});
 
    it('removes interval on unmount', ()=>{
-    vi.useFakeTimers();
+   
     const clearInterval = vi.fn();
     vi.stubGlobal('clearInterval', clearInterval);
 
     const {unmount} = render(TheHeadLine);
     unmount();
     expect(clearInterval).toHaveBeenCalled();
-    vi.useRealTimers();
+   
 
    })
    
