e9dbcb349af008599fd312cd52af685f5d66f29e
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-07-24 23:17:57 +0200

    fetched data with async await

--- search-job/src/components/JobListings.vue
+++ search-job/src/components/JobListings.vue
@@ -1,5 +1,20 @@
 <script setup>
 import JobListing from '@/components/JobListing.vue'
+import axios from 'axios';
+import { onMounted } from 'vue';
+
+const url="http://localhost:3000/jobs"
+
+const fetchJobs = async()=>{
+    const response = await axios.get(url);
+    console.log(response.data);
+};
+
+onMounted(()=>{
+    fetchJobs();
+});
+
+
 </script>
 <template>
     <main class="flex-auto bg-brand-gray-2 p-8">
--- search-job/src/playground.js
+++ search-job/src/playground.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const url= "http://localhost:3000/jobs"
 
-axios.get(url).then((response)=>{
-   console.log(response.data);
-})
+const fetchJobs = async()=>{
+    const response = await axios.get(url);
+    console.log(response.data);
+};
+
+fetchJobs();
