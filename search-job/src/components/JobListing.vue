b48f7615c03d451b1c267b50da25f85f227a67fe
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-07-25 20:35:48 +0200

    updated

--- search-job/src/components/JobListing.vue
+++ search-job/src/components/JobListing.vue
@@ -18,16 +18,22 @@ const props = defineProps({
     >
       <div class="mx-8 border-b border-solid border-brand-gray-2 pb-2 pt-5">
         <h2 class="mb-2 text-2xl">
-          Technical Program Manager, Perception, Augmented Reality
+          {{ job.title }}
         </h2>
 
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>SAP</span>
+            <span>{{ job.organization }}</span>
           </div>
 
           <div>
-            <span>San Francisco, CA, USA</span>
+            <ul>
+              <li v-for="location in job.location" :key="location" class="inline-block mr-5">
+                <span>
+                     {{ location }}
+                </span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -37,11 +43,8 @@ const props = defineProps({
           <h3 class="mb-2 mt-1">Qualifications:</h3>
           <div>
             <ul class="list-disc pl-8">
-              <li>Bachelor's degree or equivalent practical experience.</li>
-              <li>5 years of experience in program management.</li>
-              <li>
-                Experience analyzing data through querying database (e.g., SQL)
-                using spreadsheet software, and creating statistical models.
+              <li v-for="qualification in job.minimumQualifications" :key="qualification">
+                 {{ qualification }}
               </li>
             </ul>
           </div>
