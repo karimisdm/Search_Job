a3b2102a3ba11c209867ee76d5d7e3bfb999f7bf
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-11 23:29:38 +0200

    added next and previous button for going other pages

--- search-job/src/components/JobListings.vue
+++ search-job/src/components/JobListings.vue
@@ -21,11 +21,17 @@ onMounted(()=>{
 
 const currentPage = computed(()=>{
     return route.query.page ? Number(route.query.page): 1;
-})
+});
+const previousPage = computed(()=>{
+    return currentPage.value > 1 ? currentPage.value - 1 :null;
+});
+const nextPage = computed(()=>{
+    return currentPage.value < Math.ceil(jobs.value.length / 10)? currentPage.value + 1 : null;
+});
 
 
 const displayJobs = computed(()=>{
-    const numberOfPages = route.query.page ? Number(route.query.page):1;
+    const numberOfPages = currentPage.value;
     const startIndex = (numberOfPages-1)*10;
     const endIndex = numberOfPages * 10;
     if(startIndex >= jobs.value.length){
@@ -45,6 +51,18 @@ const displayJobs = computed(()=>{
         <div class="mx-auto mt-8">
             <div class="flex flex-row flex-nowrap">
                 <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+                <div class="flex items-center justify-center">
+                    <router-link v-if="previousPage" :to="{name: 'JobResults', query: {page: previousPage}}"
+                     class="mx-3 text-sm font-semibold text-brand-blue-1">
+                        Previous
+                    </router-link>
+                </div>
+                <div>
+                    <router-link v-if="nextPage" :to="{name: 'JobResults', query: {page: nextPage}}"
+                     class="mx-3 text-sm font-semibold text-brand-blue-1">
+                        Next
+                    </router-link>
+                </div>
             </div>
         </div>
     </main>
