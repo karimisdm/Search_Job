9e02015b62b2cb8f7d899520d63f14693368d5cd
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-12 00:55:19 +0200

    updated JobListing.test.js

--- search-job/test/unit/components/JobListings.test.js
+++ search-job/test/unit/components/JobListings.test.js
@@ -1,44 +1,66 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/vue";
 import { RouterLinkStub } from "@vue/test-utils";
+import '@testing-library/jest-dom';
 import axios from "axios";
 
-// Mock useRoute for Composition API
+let routeQuery = { page: '1' };
+
 vi.mock('vue-router', () => ({
-  useRoute: (queryParam) => ({
-    query: { page: '1' , ...queryParam}
+  useRoute: () => ({
+    query: routeQuery
   }),
   useRouter: () => ({})
 }));
 
 import JobListings from "@/components/JobListings.vue";
-
 vi.mock("axios");
 
 describe("JobListings", () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+  });
+
   it("fetches jobs", () => {
-    axios.get.mockResolvedValue({ data: [] });
     render(JobListings, {
       global: {
-        stubs: {
-          RouterLink: RouterLinkStub,
-        },
+        stubs: { RouterLink: RouterLinkStub },
       },
     });
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("display maximum ten jobs", async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
     render(JobListings, {
       global: {
-        stubs: {
-          RouterLink: RouterLinkStub,
-        },
+        stubs: { RouterLink: RouterLinkStub },
       },
     });
-
     const jobListings = await screen.findAllByRole("listitem");
     expect(jobListings).toHaveLength(10);
+  });
+});
+
+describe("when params exclude page number", () => {
+  it("display page number 1", () => {
+    routeQuery = { page: null };
+    render(JobListings, {
+      global: {
+        stubs: { RouterLink: RouterLinkStub },
+      },
+    });
+    expect(screen.getByText("Page 1")).toBeInTheDocument();
+  });
+});
+
+describe("when params include page number", () => {
+  it("display page number from params", () => {
+    routeQuery = { page: '4' };
+    render(JobListings, {
+      global: {
+        stubs: { RouterLink: RouterLinkStub },
+      },
+    });
+    expect(screen.getByText("Page 4")).toBeInTheDocument();
   });
 });
