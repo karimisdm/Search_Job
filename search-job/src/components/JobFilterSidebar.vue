980b0c0c0c56b880fbbb9668e9bb323bdc29dfee
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-14 21:59:21 +0200

    updated jobFilterSideBar

--- search-job/src/components/JobFilterSidebar.vue
+++ search-job/src/components/JobFilterSidebar.vue
@@ -1,9 +1,21 @@
 <script setup>
+import ActionButton from '@/components/ActionButton.vue';
 </script>
 <template>
     <div
     class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
     >
-    Job Filter Sidebar section
+    <section class="pb-5">
+        <div class="flex flex-row justify-between">
+          <h3 class="my-4 text-base font-semibold">What do you want to do? </h3>
+          <div class="flex item-center text-sm">
+            <action-button text="Clear Filters" type="secondary"/>
+          </div>
+
+        </div>
+    </section>
+
+
+
     </div>
 </template>
