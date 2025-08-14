9d14246e7cc2b5672bf3ae81142e7b1b4f6c47ec
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-14 21:10:45 +0200

    updated jobListings.test

--- search-job/test/unit/components/JobListings.test.js
+++ search-job/test/unit/components/JobListings.test.js
@@ -1,9 +1,9 @@
-import { describe, it, expect, beforeEach } from "vitest";
+import { describe, it, expect} from "vitest";
 import { render, screen } from "@testing-library/vue";
 import { RouterLinkStub } from "@vue/test-utils";
 import '@testing-library/jest-dom';
-import axios from "axios";
-
+import { createTestingPinia } from "@pinia/testing";
+import { useJobsStore } from "@/stores/jobs.js";
 let routeQuery = { page: '1' };
 
 vi.mock('vue-router', () => ({
@@ -14,38 +14,46 @@ vi.mock('vue-router', () => ({
 }));
 
 import JobListings from "@/components/JobListings.vue";
-vi.mock("axios");
+
 
 describe("JobListings", () => {
-  beforeEach(() => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
-  });
-
+ 
   it("fetches jobs", () => {
+    const pinia = createTestingPinia();
     render(JobListings, {
       global: {
         stubs: { RouterLink: RouterLinkStub },
+        plugins: [pinia],
       },
     });
-    expect(axios.get).toHaveBeenCalledWith("http://fakeapi.com/jobs");
+
+
+    const jobsStore = useJobsStore();
+    expect(jobsStore.fetchJobsAndStore).toHaveBeenCalledWith();
   });
 
   it("display maximum ten jobs", async () => {
+    const pinia = createTestingPinia();
     render(JobListings, {
       global: {
         stubs: { RouterLink: RouterLinkStub },
+        plugins: [pinia],
       },
     });
+    const jobsStore = useJobsStore();
+    jobsStore.jobs = Array(15).fill({});
     const jobListings = await screen.findAllByRole("listitem");
     expect(jobListings).toHaveLength(10);
   });
 
   describe("when params exclude page number", () => {
   it("display page number 1", () => {
+    const pinia = createTestingPinia();
     routeQuery = { page: null };
     render(JobListings, {
       global: {
         stubs: { RouterLink: RouterLinkStub },
+        plugins: [pinia],
       },
     });
     expect(screen.getByText("Page 1")).toBeInTheDocument();
@@ -54,10 +62,12 @@ describe("JobListings", () => {
 
 describe("when params include page number", () => {
   it("display page number from params", () => {
+    const pinia = createTestingPinia();
     routeQuery = { page: '4' };
     render(JobListings, {
       global: {
         stubs: { RouterLink: RouterLinkStub },
+        plugins: [pinia],
       },
     });
     expect(screen.getByText("Page 4")).toBeInTheDocument();
@@ -66,12 +76,16 @@ describe("when params include page number", () => {
 
 describe("when user is on first page",()=>{
   it("does not display previous  page link", async()=>{
+    const pinia = createTestingPinia();
     routeQuery = {page: '1'};
     render(JobListings, {
       global: {
-        stubs:{ RouterLink: RouterLinkStub},     
+        stubs:{ RouterLink: RouterLinkStub},
+        plugins: [pinia],
       }
     });
+    const  jobStore = useJobsStore();
+    jobStore.jobs = Array(15).fill({});
     await screen.findAllByRole("listitem");
     const previousLink = screen.queryByRole('link', {name: /Previous/i});
     expect(previousLink).not.toBeInTheDocument();
@@ -79,12 +93,16 @@ describe("when user is on first page",()=>{
 });
 describe("show link to next page", ()=>{
   it("when user is on first page", async()=>{
+    const pinia = createTestingPinia();
     routeQuery = {page: '1'};
     render(JobListings, {
       global: {
-        stubs:{ RouterLink: RouterLinkStub},     
+        stubs:{ RouterLink: RouterLinkStub},
+        plugins: [pinia],
       }
     });
+    const jobStore = useJobsStore();
+    jobStore.jobs = Array(15).fill({});
     await screen.findAllByRole("listitem");
     const nextLink = screen.queryByRole('link', {name: /Next/i});
     expect(nextLink).toBeInTheDocument();
@@ -93,23 +111,31 @@ describe("show link to next page", ()=>{
 
 describe("when user is on last page", ()=>{
   it("does not show link to next page ", async()=>{
+    const pinia = createTestingPinia();
     routeQuery = {page: '2'};
     render(JobListings, {
       global: {
-        stubs:{ RouterLink: RouterLinkStub},     
+        stubs:{ RouterLink: RouterLinkStub},
+        plugins: [pinia],
       }
     });
+    const jobStore = useJobsStore();
+    jobStore.jobs = Array(15).fill({});
     await screen.findAllByRole("listitem");
     const nextLink = screen.queryByRole('link', {name: /Next/i});
     expect(nextLink).not.toBeInTheDocument();
   });
   it("show link to previous page", async()=>{
+    const pinia = createTestingPinia();
     routeQuery = {page:'2'};
     render(JobListings, {
       global: {
-        stubs: {RouterLink: RouterLinkStub}
+        stubs: {RouterLink: RouterLinkStub},
+        plugins: [pinia],
       }
     });
+    const jobStore = useJobsStore();
+    jobStore.jobs = Array(15).fill({});
     await screen.findAllByRole("listitem");
     const previousLink = screen.queryByRole('link', {name: /Previous/i});
     expect(previousLink).toBeInTheDocument();
