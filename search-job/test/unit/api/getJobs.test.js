8078707670c26de2b12a6ae9288899f5d003914b
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-14 19:10:32 +0200

    added test for getJobs

--- search-job/test/unit/api/getJobs.test.js
+++ search-job/test/unit/api/getJobs.test.js
@@ -2,11 +2,26 @@ import axios from 'axios';
 import { describe, it, expect } from 'vitest';
 import '@testing-library/jest-dom'
 import getJobs from '@/api/getJobs.js'
+import { beforeEach } from 'vitest';
+import { title } from 'process';
 
 vi.mock('axios');
  describe("getJobs", ()=>{
+    beforeEach(()=>{
+        axios.get.mockResolvedValue({
+            data: [
+                { id: 1, title: "Software Engineer" }
+            ]
+        });
+    })
     it("fetch jobs successfully", async()=>{
         await getJobs();
         expect(axios.get).toHaveBeenCalledWith('http://fakeapi.com/jobs');
-    })
+    });
+    it("extracts job from response", async()=>{
+        const jobs =await getJobs();
+        expect(jobs).toEqual([
+            {id:1 , title: "Software Engineer"}
+        ])
+    });
  })
