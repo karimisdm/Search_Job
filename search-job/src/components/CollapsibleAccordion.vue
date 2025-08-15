2c6165326dc97c5f930beb4f4db14a5bfeebc9dc
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-15 21:44:15 +0200

    added  collapse for showing some checkbox

--- search-job/src/components/CollapsibleAccordion.vue
+++ search-job/src/components/CollapsibleAccordion.vue
@@ -20,11 +20,11 @@ const selectIcon = computed(() =>
  <div class="border-b border-solid border-brand-gray-2 py-5">
     <div class="flex cursor-pointer flex-wrap items-center justify-between"
        @click="open">
-        <h3>Organizations</h3>
+        <h3 class="text-base font-semibold">Organizations</h3>
         <font-awesome-icon :icon="selectIcon"/>
     </div>
-    <div v-if="isOpen">
-        child
+    <div v-if="isOpen" class="mt-5 w-full">
+        <slot></slot>
     </div>
     
  </div>
--- search-job/src/components/JobFilterSidebar.vue
+++ search-job/src/components/JobFilterSidebar.vue
@@ -13,7 +13,32 @@ import CollapsibleAccordion from './CollapsibleAccordion.vue';
             <action-button text="Clear Filters" type="secondary"/>
           </div>
         </div>
-        <collapsible-accordion/>
+        <collapsible-accordion>
+           <div class="mt-5">
+             <fieldset>
+              <ul class="flex flex-row flex-wrap">
+                <li class="h-8 w-1/2">
+                  <input id="VueTube" type="checkbox" class="mr-3"/>
+                  <label for="VueTube">VueTube</label>
+                </li>
+
+                <li class="h-8 w-1/2">
+                  <input id="Angular" type="checkbox" class="mr-3"/>
+                  <label for="Angular">Angular</label>
+                </li>
+
+                <li class="h-8 w-1/2">
+                  <input id="Between Vue and me" type="checkbox" class="mr-3"/>
+                  <label for="Between Vue and me">Between Vue and me</label>
+                </li>
+              </ul>
+
+
+             </fieldset>
+
+
+           </div>
+        </collapsible-accordion>
     </section>
 
 
