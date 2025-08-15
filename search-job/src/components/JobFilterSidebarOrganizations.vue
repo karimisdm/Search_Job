21c3597b5066c2abfe36b76526f8a7b28c8dd064
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-16 01:13:46 +0200

    added JobFilterSidebarOrganization to separate a repeating section

---
+++ search-job/src/components/JobFilterSidebarOrganizations.vue
@@ -0,0 +1,33 @@
+<script setup>
+ import CollapsibleAccordion from "@/components/CollapsibleAccordion.vue";
+
+</script>
+<template>
+    
+      <collapsible-accordion header="Organization">
+           <div class="mt-5">
+             <fieldset>
+              <ul class="flex flex-row flex-wrap">
+                <li class="h-8 w-1/2">
+                  <input id="VueTube" type="checkbox" class="mr-3"/>
+                  <label for="VueTube">VueTube</label>
+                </li>
+
+                <li class="h-8 w-1/2">
+                  <input id="Angular" type="checkbox" class="mr-3"/>
+                  <label for="Angular">Angular</label>
+                </li>
+
+                <li class="h-8 w-1/2">
+                  <input id="Between Vue and me" type="checkbox" class="mr-3"/>
+                  <label for="Between Vue and me">Between Vue and me</label>
+                </li>
+              </ul>
+
+
+             </fieldset>
+
+
+           </div>
+        </collapsible-accordion>
+</template>
