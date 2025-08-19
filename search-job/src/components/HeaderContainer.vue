35bab5d53f7821810ee71f2493ec988bd96440f5
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-19 22:44:51 +0200

    added HeadContainer

---
+++ search-job/src/components/HeaderContainer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="w-full border-b border-solid border-brand-gray-2">
+        <div class="mx-auto my-16 text-center">
+            <slot name="title">Sample title</slot>
+            <slot name="subtitle">Sample content</slot>
+        </div>
+
+    </div>
+</template>
