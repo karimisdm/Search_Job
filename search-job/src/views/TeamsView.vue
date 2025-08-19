796ba6ca8a19b5d6c4dcc749be8939693596e80f
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-19 23:06:54 +0200

    added slots to TeamsView

--- search-job/src/views/TeamsView.vue
+++ search-job/src/views/TeamsView.vue
@@ -2,6 +2,16 @@
 import HeaderContainer from '@/components/HeaderContainer.vue';
 </script>
 <template>
-    <HeaderContainer/>
+    <header-container>
+        <template v-slot:title>
+            <h1 class="w-full text-4xl font-normal">Teams</h1>
+        </template>
+        <template v-slot:subtitle>
+            <h2 class="my-4 w-full text-sm font-light">
+                Its awesome working here. Why don't you join us?
+            </h2>
+
+        </template>
+    </header-container>
 
 </template>
