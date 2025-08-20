3439758064cb2d33c492535fcfeeb22f8b6e8feb
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-20 21:16:58 +0200

    Updated

--- search-job/src/views/TeamsView.vue
+++ search-job/src/views/TeamsView.vue
@@ -3,10 +3,10 @@ import HeaderContainer from '@/components/HeaderContainer.vue';
 </script>
 <template>
     <header-container>
-        <template v-slot:title>
+        <template #title>
             <h1 class="w-full text-4xl font-normal">Teams</h1>
         </template>
-        <template v-slot:subtitle>
+        <template #subtitle>
             <h2 class="my-4 w-full text-sm font-light">
                 Its awesome working here. Why don't you join us?
             </h2>
