dde6e2b1a58995f18aa05c2e8e1eb5aa5f3e1c98
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-22 19:11:23 +0200

    Updated JobFilterOrganization

--- search-job/src/components/JobFilterSidebarOrganizations.vue
+++ search-job/src/components/JobFilterSidebarOrganizations.vue
@@ -1,12 +1,17 @@
 <script setup>
  import CollapsibleAccordion from "@/components/CollapsibleAccordion.vue";
  import {useJobsStore} from "@/stores/jobs.js";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
  const jobsStore = useJobsStore();
  const Organizations = computed(()=>{
      return jobsStore.getOrganizationOfJobs();
  });
+ const selectedOrganizations = ref([]);
+
+ const handleChanges = ()=>{
+   console.log(selectedOrganizations.value);
+ }
 
 </script>
 <template>
@@ -16,11 +21,12 @@ import { computed } from "vue";
              <fieldset>
               <ul class="flex flex-row flex-wrap">
                 <li v-for="organization in Organizations" :key="organization" class="h-8 w-1/2">
-                  <input :id="organization" type="checkbox" class="mr-3"/>
+                  <input :id="organization" v-model="selectedOrganizations" :value="organization" type="checkbox" @change="handleChanges" class="mr-3"/>
                   <label :for="organization">{{ organization }}</label>
                 </li>
               </ul>
              </fieldset>
            </div>
         </collapsible-accordion>
+        {{ selectedOrganizations }}
 </template>
