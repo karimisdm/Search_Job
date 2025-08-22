f5341bf908977d08dd41774ee3f4d499f93e5947
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-08-22 22:32:17 +0200

    Updated use.js

--- search-job/src/stores/user.js
+++ search-job/src/stores/user.js
@@ -4,10 +4,21 @@ import { ref } from 'vue';
 export const useUserStore = defineStore('user',()=>{
 
     const isLoggedIn = ref(false);
+    const selectedOrganizations = ref([]);
     
     const loginUser = ()=>{
         isLoggedIn.value = true;
     }
+    const AddSelectedOrganization = (organization)=>{
+        if(!selectedOrganizations.value.includes(organization)){
+           selectedOrganizations.value.push(organization);
+        }
+        
+    }
 
-    return { isLoggedIn, loginUser }
+    return { isLoggedIn,
+         loginUser, 
+         selectedOrganizations,
+        AddSelectedOrganization,
+     }
 })
